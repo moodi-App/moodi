a3b0dec82618aff3baed545b03cf9ec5d81a849d
Esther Witbeck <esther.witbeck@gmail.com>
2023-06-06 15:51:52 -0700

    add 404 router
    include 404.html; confirm passing test
    leave bugs in absence of __dirname
    
    Co-authored-by: jhosea92 <jhosea92@users.noreply.github.com>

--- __tests__/server/server.test.ts
+++ __tests__/server/server.test.ts
@@ -1,10 +1,13 @@
+/**
+ * @jest-environment node
+ */
 const request = require('supertest');
 
 import app from '../../src/server/server'
 
 afterAll(() => {
     app.close();
-})
+});
 
 describe('Basic Error Handling', () => {
     describe('404 Route', () => {
@@ -15,6 +18,25 @@ describe('Basic Error Handling', () => {
                 .expect(404, done);
         });
     });
+    describe('Global Error Handler', () => {
+        it('responds with 500 to errors within the server', (done) => {
+            request(app)
+                .get('/broken')
+                .expect('Content-Type', /json/)
+                .expect(500, done);
+        });
+        it('logs a server-side error', (done) => {
+            const log = jest.spyOn(console, 'log');
+            expect(log.mock.calls.length).toBe(0);
+            request(app)
+                .get('/broken');
+            expect(log.mock.calls.length).toBe(1);
+            jest.clearAllMocks();
+            return done();
+        })
+    })
 });
+
+
 
 
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,19 @@
 import express, { Request, Response, NextFunction } from 'express'; 
+import path from 'path';
+
+
 const app = express();
 
 app.use(express.json());
 
 //404 handler
 app.use((req: Request, res: Response, next: NextFunction): void => {
+  res.status(404).sendFile(path.resolve('./assets/404.html'), (err) => {
+    if (err) return next(err);
+  });
 });
 
+
+
+// listen returns server instance; needed for test
 export default app.listen(3000, () => console.log('listening on port 3000'));
