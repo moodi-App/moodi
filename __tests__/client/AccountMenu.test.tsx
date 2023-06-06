d38b8a27f278641ffe6d9bac3936223ae3626417
Megan Choi <meganchoi732@gmail.com>
2023-06-06 16:25:20 -0700

    test for logout button in AccountMenu

--- __tests__/client/AccountMenu.test.tsx
+++ __tests__/client/AccountMenu.test.tsx
@@ -3,13 +3,26 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import AccountMenu from '../../src/client/components/AccountMenu';
 import '@testing-library/jest-dom';
 
+// mock useNavigate to create spy function
+const navigate = jest.fn();
+jest.mock("react-router-dom", () => ({
+  useNavigate: () => navigate,
+}));
+
 beforeEach(() => {
   render(<AccountMenu />);
 });
+
 describe('AccountMenu', () => {
   test('contains avatar and logout button', () => {
     expect(screen.getByRole('img')).toBeInTheDocument();
     expect(screen.getByRole('button')).toHaveTextContent('Logout');
+  });
+
+  test('logout button redirects to login page', () => {
+    const logoutButton = screen.getByRole('button');
+    fireEvent.click(logoutButton);
+    expect(navigate).toHaveBeenCalledWith("/login");
   });
 
 });
