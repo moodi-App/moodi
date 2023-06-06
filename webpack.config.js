88618ff0990a27e3d6908e6210eae7bd8dee220d
Eric Tjon <ectjon@gmail.com>
2023-06-06 12:15:02 -0700

    Add tailwind css

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,8 @@ module.exports = {
         test: /\.css$/,
         use: [
           'style-loader',
-          'css-loader'
+          'css-loader',
+          'postcss-loader'
         ]
       },
     ],
