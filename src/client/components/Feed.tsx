b8494327273b88dbce241ed09e5d918dff8b19a3
Eric Tjon <ectjon@gmail.com>
2023-06-07 10:38:55 -0700

    Add basic postcard and feed with sample data

--- src/client/components/Feed.tsx
+++ src/client/components/Feed.tsx
@@ -3,12 +3,22 @@ import PostCard from "./PostCard";
 import PostForm from "./PostForm";
 import { getPostList } from "../api"
 export default function Feed(){
-  
+  // need to fetch post data array and map to post card components
+  //sample card
+  const sample_post = {
+    username: 'Eric',
+    user_img: 'https://trello.com/1/cards/647eaaf3e02c15a37fa00a77/attachments/647f67fe58420da644e7c43b/download/Mooder-sq.png',
+    emoji: 0,
+    scale: 5,
+    text: 'Feeling great',
+    timestamp: '10am',
+    reaction: 2
+  }
   return(
     <div>
       Feed
       <PostForm/>
-      <PostCard/>
+      <PostCard {...sample_post}/>
     </div>
   )
 }
